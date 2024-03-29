5576d681452d1f823cb5940f427517d37406ab3f
Daniil Chumakin <145126478+Pryduck@users.noreply.github.com>
2024-03-29 23:13:59 +0700

    Final Check

--- task_1/src/main/java/org/task1/Check.java
+++ task_1/src/main/java/org/task1/Check.java
@@ -1,23 +1,42 @@
 package org.task1;
 
 public class Check {
+    private final int number;
+    private final char[] str;
+
+    public char[] getStr() {
+        return str;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Check(int number) {
+        this.number = number;
+
+        String strNumb = Integer.toString(number);
+        this.str = strNumb.toCharArray();
+    }
+
+
     //функция проверки размера числа
-    public static int sizeCheck(int numb) {
-        if ((numb < 1000) || (numb > 9999)) {
-            System.out.println("your number is out of range, try again: ");
-            return 0;
-        } else return 1;
+    public boolean sizeCheck() {
+        if ((number < 1000) || (number > 9999)) {
+            return false;
+        }
+        return true;
     }
 
 
     //функция проверки разных цифр
-    public static int different(char[] c) {
-        for (int i = 0; i < c.length - 1; i++) {
-            for (int j = i+1; j < c.length; j++) {
-                if (c[i] == c[j])
-                    return 0;
+    public boolean different() {
+        for (int i = 0; i < str.length - 1; i++) {
+            for (int j = i+1; j < str.length; j++) {
+                if (str[i] == str[j])
+                    return false;
             }
         }
-        return 1;
+        return true;
     }
 }
