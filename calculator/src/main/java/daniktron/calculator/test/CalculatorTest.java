64e193073b8c5ae2dcc3cc5b2cf91fffe498d2e6
Daniil Chumakin <145126478+Pryduck@users.noreply.github.com>
2024-06-05 11:19:18 +0700

    Update CalculatorTest.java

--- calculator/src/main/java/daniktron/calculator/test/CalculatorTest.java
+++ calculator/src/main/java/daniktron/calculator/test/CalculatorTest.java
@@ -49,104 +49,94 @@ public class CalculatorTest {
 
     private static final TestDescriptor[] tests = {
         new TestDescriptor(
-            """
-            #test 1
-            #testing PRINT, POP and PUSH operations
-                        
-            PUSH 1
-            PRINT
-            PUSH 2
-            PRINT
-                        
-            POP
-            PRINT
-            
-            """,
+
+            "#test 1\n"+
+            "#testing PRINT, POP and PUSH operations\n"+
+
+            "PUSH 1\n"+
+            "PRINT\n"+
+            "PUSH 2\n"+
+            "PRINT\n"+
+
+            "POP\n"+
+            "PRINT\n",
             "1.0\n2.0\n1.0\n"
 
         ),
         new TestDescriptor(
-            """
-            #test 2
-            #testing POP on empty stack
-                        
-            PUSH 1
-            PUSH 2
-                        
-            POP
-            POP
-            POP
-            
-            """,
+
+            "#test 2\n"+
+            "#testing POP on empty stack\n"+
+
+            "PUSH 1\n"+
+            "PUSH 2\n"+
+
+            "POP\n"+
+            "POP\n",
             "",
             StackCalcTooShortStackSizeException.class
         ),
         new TestDescriptor(
-            """
-            #test 3
-            #testing how "+" and "-" operations
-            
-            PUSH 10
-            PUSH 40
-            +
-            PRINT
-            #answer: 50
-            
-            
-            PUSH 10
-            PUSH 40
-            -
-            PRINT
-            #answer: 30
-            
-            """,
+
+            "#test 3\n"+
+            "#testing how \"+\" and \"-\" operations\n"+
+
+            "PUSH 10\n"+
+            "PUSH 40\n"+
+            "+\n"+
+            "PRINT\n"+
+            "#answer: 50\n"+
+
+
+            "PUSH 10\n"+
+            "PUSH 40\n"+
+            "-\n"+
+            "PRINT\n"+
+            "#answer: 30\n",
             "50.0\n30.0\n"
         ),
         new TestDescriptor(
-            """
-            #test 4
-            #testing DEFINE operation
-                        
-            DEFINE a 10
-            PUSH a
-            PRINT
-                        
-            DEFINE b 20
-            PUSH b
-            PRINT
-                        
-            DEFINE c b
-            PUSH c
-            PRINT
-                        
-            DEFINE d abracadabra
-            PUSH d
-            PRINT
-            """,
+
+            "#test 4\n"+
+            "#testing DEFINE operation\n"+
+
+            "DEFINE a 10\n"+
+            "PUSH a\n"+
+            "PRINT\n"+
+
+            "DEFINE b 20\n"+
+            "PUSH b\n"+
+            "PRINT\n"+
+
+            "DEFINE c b\n"+
+            "PUSH c\n"+
+            "PRINT\n"+
+
+            "DEFINE d abracadabra\n"+
+            "PUSH d\n"+
+            "PRINT\n",
             "10.0\n20.0\n20.0\n",
             StackCalcVariableNotDefinedException.class
         ),
         new TestDescriptor(
-            """
-            #test 5
-            #testing SQRT
-                        
-            PUSH 1
-            SQRT
-            PRINT
-                        
-            PUSH 4
-            SQRT
-            PRINT
-                        
-            DEFINE value 9
-            PUSH value
-            SQRT
-            PRINT
-            """,
-            "1.0\n2.0\n3.0\n"
-        )
 
+            "#test 5\n"+
+            "#testing SQRT\n"+
+
+            "PUSH 1\n"+
+            "SQRT\n"+
+            "PRINT\n"+
+
+            "PUSH 4\n"+
+            "SQRT\n"+
+            "PRINT\n"+
+
+            "DEFINE value 9\n"+
+            "PUSH value\n"+
+            "SQRT\n"+
+            "PRINT\n",
+            "1.0\n2.0\n3.0\n"
+        ),
     };
 
     @Test
