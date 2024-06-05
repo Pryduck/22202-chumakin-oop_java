db25442414ccb11227711d769a8f937ec83badd4
Daniil Chumakin <145126478+Pryduck@users.noreply.github.com>
2024-06-05 09:44:28 +0700

    Update Variable.java

--- calculator/src/main/java/daniktron/calculator/Variable.java
+++ calculator/src/main/java/daniktron/calculator/Variable.java
@@ -6,7 +6,6 @@ import daniktron.calculator.exceptions.StackCalcVariableNotDefinedException;
 import java.util.Map;
 //токен -> число, getVal  все операции с токенами
 public class Variable {
-    //строка, которая хар-ет переменную - имя переменной или значение
     private final String val;
     //предопределяет во время вызова конструктора - подана ли на вход строка или число
     private final boolean isConst;
@@ -21,7 +20,7 @@ public class Variable {
         }
         return f;
     }
-    //конструктор
+    
     public Variable(final String val) {
         this.val = val;
         this.isConst = isConst(val);
@@ -31,15 +30,15 @@ public class Variable {
         return this.isConst;
     }
     public final String getName() { return this.val; }
-    //проверка - число ли это (в конструкторе например её вызываем)
+    //проверка - число ли это
     public static boolean isConst(final String val) {
-        char c = val.charAt(0); //получить символ с индексом ноль (как массив [0]
+        char c = val.charAt(0);
         return ('0' <= c) && (c <= '9') || (c == '-');
     }
 
     public static float parse(final String val) throws StackCalcConstParseException {
         float f;
-        try { //ловится ошибка во время парсинга
+        try { 
             f = Float.parseFloat(val);
         }
         catch (NumberFormatException e) {
